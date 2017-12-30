6da7b2a5f54c5b340a38d270f220aae623c35e70
wlw81 <thomas.pasligh@gmail.com>
2017-12-30 22:55:19 +0100

    now able to display random images

--- app/src/main/java/de/pasligh/android/hauptkanalmemory/GameActivity.java
+++ app/src/main/java/de/pasligh/android/hauptkanalmemory/GameActivity.java
@@ -1,20 +1,29 @@
 package de.pasligh.android.hauptkanalmemory;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -32,25 +41,22 @@ public class GameActivity extends AppCompatActivity {
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    public String[] getImageNames() {
+        if (null == imageNames) {
+            try {
+                imageNames = getAssets().list("hauptkanalLinks");
+            } catch (IOException e) {
+                Log.e("BLA", e.getMessage());
+            }
         }
-    };
+
+        return imageNames;
+    }
+
+    private int currentNumber = 0;
+    private String[] imageNames = null;
+    private View mContentView;
     private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
@@ -70,20 +76,6 @@ public class GameActivity extends AppCompatActivity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,21 +89,70 @@ public class GameActivity extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.GameContentLayout);
-        mContentView = findViewById(R.id.fullscreen_content);
 
+        ((ImageButton) findViewById(R.id.imageButtonOption1)).setOnClickListener(this);
+        ((ImageButton) findViewById(R.id.imageButtonOption2)).setOnClickListener(this);
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+        Toast.makeText(getApplicationContext(), "Irgendwas mit Aliens", Toast.LENGTH_LONG).show();
+        displayHousenumber(currentNumber);
     }
+
+    public int generateHousenumber() {
+        Random myGenerator = new Random();
+        int houseNumber = currentNumber;
+        while (houseNumber == currentNumber) {
+            houseNumber = myGenerator.nextInt(getImageNames().length);
+        }
+        return houseNumber;
+    }
+
+    public void displayHousenumber(int p_number) {
+        try {
+            ((ImageView) findViewById(R.id.imageView)).setImageDrawable(getImage(p_number));
+
+            Bitmap bitmapHouseBefore = scaleBitmap(generateHousenumber());
+            ((ImageButton) findViewById(R.id.imageButtonOption1)).setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
+                    , bitmapHouseBefore));
+
+
+            Bitmap bitmapHouseAfter = scaleBitmap(generateHousenumber());
+            ((ImageButton) findViewById(R.id.imageButtonOption2)).setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
+                    , bitmapHouseAfter));
+        } catch (Exception e) {
+            Log.e("BLA", e.getMessage());
+        }
+    }
+
+    private Bitmap scaleBitmap(int p_number) {
+        BitmapDrawable bd = (BitmapDrawable) getImage(p_number);
+        return Bitmap.createScaledBitmap(bd.getBitmap(),
+                (int) (bd.getIntrinsicHeight() * 0.8),
+                (int) (bd.getIntrinsicWidth() * 0.8),
+                false);
+    }
+
+    public Drawable getImage(int p_number) {
+        Drawable imageReturn = null;
+        InputStream inputstream = null;
+        try {
+            inputstream = getApplicationContext().getAssets().open("hauptkanalLinks/" + getImageNames()[p_number]);
+            imageReturn = Drawable.createFromStream(inputstream, null);
+        } catch (IOException e) {
+            Log.e("BLA", e.getMessage());
+        } finally {
+            if (null != inputstream) {
+                try {
+                    inputstream.close();
+                    inputstream = null;
+                } catch (IOException e) {
+                    Log.e("BLA", e.getMessage());
+                }
+            }
+        }
+
+        return imageReturn;
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -120,7 +161,6 @@ public class GameActivity extends AppCompatActivity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
-        delayedHide(100);
     }
 
     @Override
@@ -151,9 +191,6 @@ public class GameActivity extends AppCompatActivity {
         mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
     @SuppressLint("InlinedApi")
@@ -163,17 +200,11 @@ public class GameActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         mVisible = true;
 
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    @Override
+    public void onClick(View v) {
+        currentNumber++;
+        displayHousenumber(currentNumber);
     }
 }
