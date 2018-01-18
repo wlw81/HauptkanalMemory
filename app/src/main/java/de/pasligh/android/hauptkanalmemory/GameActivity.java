1ec91112850e6d63ba228b02724aba48bbdcf895
wlw81 <thomas.pasligh@gmail.com>
2018-01-18 22:43:06 +0100

    resource management via async tasks

--- app/src/main/java/de/pasligh/android/hauptkanalmemory/GameActivity.java
+++ app/src/main/java/de/pasligh/android/hauptkanalmemory/GameActivity.java
@@ -1,18 +1,21 @@
 package de.pasligh.android.hauptkanalmemory;
 
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
@@ -24,6 +27,10 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -42,10 +49,26 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     int spSwoosh;
     int spSuck;
     SoundPool soundPool;
+
+    private Drawable nextHouse;
+    private Bitmap bitmapHouseAfter;
+
+    private Map<ImageButton, CardView> mapCardViews = new HashMap<>();
+    private List<Bitmap> listRandomImages = new ArrayList<Bitmap>();
+
     ImageButton imgButton1;
     ImageButton imgButton2;
     ImageButton imgButton3;
+
+    CardView crdView1;
+    CardView crdView2;
+    CardView crdView3;
+
     Handler myHandler;
+
+    Animation slideUp1;
+    Animation slideUp2;
+    Animation slideUp3;
 
     /**
      * Whether or not the system UI should be auto-hidden after
@@ -65,8 +88,28 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
      */
     private static final int UI_ANIMATION_DELAY = 300;
 
+    private int lastRandomHouseNumber = -1;
     private int currentNumber = 0;
     private String currentStreet;
+
+    private class SwooshListener implements Animation.AnimationListener {
+
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            soundPool.play(spSwoosh, 1, 1, 0, 0, 1);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
 
     public String[] getImageNames() {
         if (null == imageNames) {
@@ -93,8 +136,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    String countdown = String.format("%02d:%02d:%02d",
-                            TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                    String countdown = String.format("%02d:%02d",
                             TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
                                     TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)), // The change is in this line
                             TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
@@ -138,6 +180,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         myHandler = new Handler();
+        Animation.AnimationListener mySwooshListner = new SwooshListener();
         currentStreet = getIntent().getStringExtra(Flags.STREET);
         mControlsView = findViewById(R.id.GameContentLayout);
 
@@ -145,12 +188,33 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         imgButton2 = (ImageButton) findViewById(R.id.imageButtonOption2);
         imgButton3 = (ImageButton) findViewById(R.id.imageButtonOption3);
 
+        crdView1 = (CardView) findViewById(R.id.cardViewOption1);
+        crdView2 = (CardView) findViewById(R.id.cardViewOption2);
+        crdView3 = (CardView) findViewById(R.id.cardViewOption3);
+
+        mapCardViews.put(imgButton1, crdView1);
+        mapCardViews.put(imgButton2, crdView2);
+        mapCardViews.put(imgButton3, crdView3);
+
         imgButton1.setOnClickListener(this);
         imgButton2.setOnClickListener(this);
         imgButton3.setOnClickListener(this);
 
+        slideUp1 = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_up);
+        slideUp2 = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_up);
+        slideUp3 = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_up);
+
+        slideUp2.setStartOffset(Flags.DELAY);
+        slideUp3.setStartOffset(Flags.DELAY * 2);
+
+        slideUp1.setAnimationListener(mySwooshListner);
+        slideUp2.setAnimationListener(mySwooshListner);
+        slideUp3.setAnimationListener(mySwooshListner);
+
+        refillImages();
 
         displayHousenumber(currentNumber);
+        displayScore();
         soundPool = new SoundPool.Builder().build();
 
         spBad = soundPool.load(this, R.raw.bad, 1);
@@ -160,92 +224,73 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         getTimer().start();
     }
 
+    private void refillImages() {
+        Log.i(Flags.LOG, "Refilling images, currently " + listRandomImages.size() + " in stock.");
+        for (int i = 0; i < Flags.RANDOM_IMAGES; i++) {
+            listRandomImages.add(scaleBitmap(generateHousenumber()));
+        }
+        nextHouse = getImage(currentNumber + 1);
+        bitmapHouseAfter = scaleBitmap(currentNumber + 2);
+    }
+
     public int generateHousenumber() {
         Random myGenerator = new Random();
         int houseNumberGenerated = currentNumber;
-        while (houseNumberGenerated == currentNumber || houseNumberGenerated == (currentNumber + 1)) {
+        while (houseNumberGenerated == currentNumber || houseNumberGenerated == (currentNumber + 1) || houseNumberGenerated == lastRandomHouseNumber) {
             houseNumberGenerated = myGenerator.nextInt(getImageNames().length);
         }
+        lastRandomHouseNumber = houseNumberGenerated; // not the same number next time
         return houseNumberGenerated;
     }
 
     public void displayHousenumber(int p_number) {
         try {
-            ((ImageView) findViewById(R.id.imageView)).setImageDrawable(getImage(p_number));
-
-            Bitmap bitmapHouseAfter = scaleBitmap(currentNumber + 1);
-
+            ((ImageView) findViewById(R.id.imageView)).setImageDrawable(nextHouse);
             int random = new Random().nextInt(3);
 
-            imgButton1.setImageDrawable(null);
-            imgButton2.setImageDrawable(null);
-            imgButton3.setImageDrawable(null);
-
-            switch (random) {
-                case 0:
-                    imageButtonCorrectChoice = ((ImageButton) findViewById(R.id.imageButtonOption1));
-                    imageButtonCorrectChoice.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
-                            , bitmapHouseAfter));
-                    break;
-                case 1:
-                    imageButtonCorrectChoice = ((ImageButton) findViewById(R.id.imageButtonOption2));
-                    imageButtonCorrectChoice.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
-                            , bitmapHouseAfter));
-                    break;
-                case 2:
-                    imageButtonCorrectChoice = ((ImageButton) findViewById(R.id.imageButtonOption3));
-                    imageButtonCorrectChoice.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
-                            , bitmapHouseAfter));
-                    break;
+            ImageButton[] imageButtons = new ImageButton[]{imgButton1, imgButton2, imgButton3};
+            for (ImageButton imgbtn : imageButtons) {
+                imgbtn.setImageDrawable(null);
             }
 
-            if (null == imgButton1.getDrawable()) {
-                imgButton1.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources(), scaleBitmap(generateHousenumber())));
-            }
+            imageButtonCorrectChoice = imageButtons[random];
+            imageButtonCorrectChoice.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources()
+                    , bitmapHouseAfter));
 
-            if (null == imgButton2.getDrawable()) {
-                imgButton2.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources(), scaleBitmap(generateHousenumber())));
-            }
 
-            if (null == imgButton3.getDrawable()) {
-                imgButton3.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources(), scaleBitmap(generateHousenumber())));
-            }
-
-            final Animation slideUp = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_up);
-            imgButton1.setVisibility(View.VISIBLE);
-            imgButton1.startAnimation(slideUp);
-            soundPool.play(spSwoosh, 1, 1, 0, 0, 1);
-            slideUp.setStartOffset(200);
-            imgButton2.startAnimation(slideUp);
-            myHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    imgButton2.setVisibility(View.VISIBLE);
-                    soundPool.play(spSwoosh, 1, 1, 0, 0, 1);
-
+            for (ImageButton imgbtn : imageButtons) {
+                if (null == imgbtn.getDrawable()) {
+                    Bitmap nextRandomImage = listRandomImages.iterator().next();
+                    imgbtn.setImageDrawable(new BitmapDrawable(getApplicationContext().getResources(), nextRandomImage));
+                    listRandomImages.remove(nextRandomImage);
                 }
-            }, 150);
+                mapCardViews.get(imgbtn).setVisibility(View.VISIBLE);
+            }
 
-            slideUp.setStartOffset(400);
-            imgButton3.startAnimation(slideUp);
-            myHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    imgButton3.setVisibility(View.VISIBLE);
-                    soundPool.play(spSwoosh, 1, 1, 0, 0, 1);
-                }
-            }, 350);
+            crdView1.startAnimation(slideUp1);
+            crdView2.startAnimation(slideUp2);
+            crdView3.startAnimation(slideUp3);
+
 
         } catch (Exception e) {
             Log.e(LOG, e.getMessage());
+        } finally {
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    refillImages();
+                }
+            });
         }
     }
 
     private Bitmap scaleBitmap(int p_number) {
         BitmapDrawable bd = (BitmapDrawable) getImage(p_number);
+        float ht_px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, bd.getIntrinsicHeight(), getResources().getDisplayMetrics());
+        float wt_px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, bd.getIntrinsicWidth(), getResources().getDisplayMetrics());
         return Bitmap.createScaledBitmap(bd.getBitmap(),
-                (int) (bd.getIntrinsicWidth() * 0.9),
-                (int) (bd.getIntrinsicHeight() * 0.9),
+                (int) wt_px / 3,
+                (int) ht_px / 3,
                 false);
     }
 
@@ -298,23 +343,26 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         final Animation scaleAnim = AnimationUtils.loadAnimation(this, R.anim.animate);
         final Animation slideDown = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_down);
         final Animation slideDownFast = AnimationUtils.loadAnimation(GameActivity.this, R.anim.slide_down);
-        if(imageButtonCorrectChoice != imgButton1){
-            imgButton1.startAnimation(slideDown);
-            soundPool.play(spSuck, 1, 1, 0, 0, 1);
-            imgButton1.setVisibility(View.GONE);
-        }
-        if(imageButtonCorrectChoice != imgButton2){
-            imgButton2.startAnimation(slideDown);
-            soundPool.play(spSuck, 1, 1, 0, 0, 1);
-            imgButton2.setVisibility(View.GONE);
-        }
-        if(imageButtonCorrectChoice != imgButton3){
-            imgButton3.startAnimation(slideDown);
-            soundPool.play(spSuck, 1, 1, 0, 0, 1);
-            imgButton3.setVisibility(View.GONE);
 
+        displayScore();
+
+        if (imageButtonCorrectChoice != imgButton1) {
+            crdView1.startAnimation(slideDown);
+            crdView1.setVisibility(View.GONE);
+            soundPool.play(spSuck, 1, 1, 0, 0, 1);
         }
-        imageButtonCorrectChoice.startAnimation(scaleAnim);
+        if (imageButtonCorrectChoice != imgButton2) {
+            crdView2.startAnimation(slideDown);
+            crdView2.setVisibility(View.GONE);
+            soundPool.play(spSuck, 1, 1, 0, 0, 1);
+        }
+        if (imageButtonCorrectChoice != imgButton3) {
+            crdView3.startAnimation(slideDown);
+            crdView3.setVisibility(View.GONE);
+            soundPool.play(spSuck, 1, 1, 0, 0, 1);
+        }
+
+        mapCardViews.get(imageButtonCorrectChoice).startAnimation(scaleAnim);
         scaleAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -327,7 +375,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     finish();
                 } else {
                     soundPool.play(spSuck, 1, 1, 0, 0, 1);
-                    imageButtonCorrectChoice.startAnimation(slideDownFast);
+                    mapCardViews.get(imageButtonCorrectChoice).startAnimation(slideDownFast);
                     slideDownFast.setAnimationListener(new Animation.AnimationListener() {
                         @Override
                         public void onAnimationStart(Animation animation) {
@@ -354,6 +402,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+    }
+
+    private void displayScore() {
+        final Animation bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        ((TextView) findViewById(R.id.textViewScore)).setText(String.valueOf(score) + " " + getString(R.string.score));
+        findViewById(R.id.cardViewScore).startAnimation(bounce);
     }
 
     @Override
--- app/src/main/java/de/pasligh/android/tools/Flags.java
+++ app/src/main/java/de/pasligh/android/tools/Flags.java
@@ -10,6 +10,8 @@ public final class Flags {
     public final static String STREET_LEFT = "hauptkanalLinks";
     public final static String STREET = "SEITE";
     public final static String LOG = "HAUPTKANAL";
-    public final static int COUNTDOWN = 90000;
+    public final static int COUNTDOWN = 120000;
+    public final static int DELAY = 600;
+    public final static int RANDOM_IMAGES = 2;
 
 }
